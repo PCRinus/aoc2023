839806647804a85273b44004830e71c3ce9ee7ed
Mircea Casapu <mircea.casapu@gmail.com>
2023-12-01 11:05:59 +0200

    feat: day01 part1

--- src/day01/part1.ts
+++
@@ -1,7 +0,0 @@
-// import { readFile } from "../utils/readInput";
-
-// const input = await readFile('./input.txt')
-
-// console.log(input)
-
-console.log('Hello');
---
+++ src/day01/part1/index.ts
@@ -0,0 +1,43 @@
+import { readFileLineByLine } from "../../utils/readInput";
+
+const getCalibrationValue = (line: string): string => {
+  let param1 = "";
+  let param2 = "";
+  for (let i = 0; i <= line.length; i++) {
+    if (param1 && param2) break;
+    if (param1 === "" && Number.isInteger(parseInt(line[i]))) {
+      param1 = line[i];
+    }
+
+    if (param2 === "" && Number.isInteger(parseInt(line[line.length - i]))) {
+      param2 = line[line.length - i];
+    }
+  }
+
+  console.log(param1 + param2);
+
+  return param1 + param2;
+};
+
+const getTotalValue = (values: string[]) => {
+  const total = values.reduce((acc, curr) => {
+    return acc + +curr;
+  }, 0);
+
+  return total;
+};
+
+const input = await readFileLineByLine("./src/day01/part1/input.txt");
+const values: string[] = [];
+
+
+input.forEach((line) => {
+  const calibrationValue = getCalibrationValue(line);
+  values.push(calibrationValue);
+});
+
+console.log("VALUES", values);
+
+const total = getTotalValue(values);
+
+console.log("TOTAL", total);
